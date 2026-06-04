63cf0a2278c2d46b3bae9c5bc94f530b673535f7
yushmeh <166646788+yushmeh@users.noreply.github.com>
2026-06-04 13:33:34 +0300

    Delete MiningSimulator directory

--- MiningSimulator/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.secret_key = 'super-secret-mining-key-123' # Секретный ключ обязателен для работы сессий игрока!
-
-# Импорт роутов пишется в самом конце, когда объект app уже создан
-from app import routes
